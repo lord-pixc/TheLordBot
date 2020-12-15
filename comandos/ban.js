ef17697196925c69fc5d99b77530243c97f6c69d
Elian Corea <eliancorea2317@gmail.com>
2020-12-14 21:20:14 -0600

    Update ban.js

--- comandos/ban.js
+++ comandos/ban.js
@@ -4,13 +4,16 @@ module.exports = {
     run: async (client, message, args) => {
         if (!message.guild) return;
         const user = message.mentions.users.first();
-        if (message.member.hasPermission('BAN_MEMBERS')){ return 'No tienes permisos';}
+        if (message.guild.member(message.author).hasPermission('BAN_MEMBERS')){ return 'No tienes permisos';}
         if (user) {
           const member = message.guild.member(user);
           if (member) {  
             member
               .ban({
                 reason: 'Desiciones del staff',
+              })
+              .has({
+                permission: ('BAN_MEMBERS')
               })
               .then(() => {
                 message.reply(`A sido baneado ${user.tag}`);
