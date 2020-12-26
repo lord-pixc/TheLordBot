ddf9030e603e644bef1cbb05d620f51a714f64d3
lord-pixc <74274540+lord-pixc@users.noreply.github.com>
2020-12-26 16:18:25 -0600

    Update invite.js

--- comandos/invite.js
+++ comandos/invite.js
@@ -4,6 +4,6 @@ module.exports = {
     nombre: "invite",
     alias: ["invitar"],
     run: async (client, message, args) => {
-        message.channel.send("Puedes invitarme a tu serer desde https://cutt.ly/2hUGlED ```Sin miedo al acortador```")
+        message.channel.send("Puedes invitarme a tu server desde https://cutt.ly/2hUGlED ```Sin miedo al acortador```")
     }
 }
