be4d08f839a1f2760cf8713c4a9d832dbbaceaeb
lord-pixc <74274540+lord-pixc@users.noreply.github.com>
2020-12-27 01:48:44 -0600

    final unban

--- comandos/unban.js
+++ comandos/unban.js
@@ -2,18 +2,16 @@ module.exports = {
   nombre: "unbanmantenimiento",
   alias: [],
   run: async (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR" )) return message.channel.send("no tienes permisos")
       if (!message.guild) return;
       const user = message.mentions.users.first();
       if (user) {
         const member = message.guild.member(user);
         if (member) {  
           member
-            .ban({
+            .unban({
               reason: 'Desiciones del staff',
             })
-            .has(('BAN_MEMBERS'),
-              message.reply('No tienes permisos')
-            )
             .then(() => {
               message.reply(`A sido desbaneado ${user.tag}`);
             })
