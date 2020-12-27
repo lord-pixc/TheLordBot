25266bc7714f091bc224b4167c300fa2459c8d61
lord-pixc <74274540+lord-pixc@users.noreply.github.com>
2020-12-27 14:03:48 -0600

    add say

---
+++ comandos/say.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Nombre: "say",
+    Alias: [],
+    run: async (client, message, args) => {
+        message.delete();
+        if(!args[0]) return message.channel.send("Escribe lo que quieras que diga").then(m => m.delete({ timeout: 5000 }));
+        const texto = args.join(" ")
+        message.channel.send(texto, { allowedMentions: { parse: [] } })
+            
+    }
+}
