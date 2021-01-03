dc4d142c91eaba94df6b61ea1e48cfa6dff76ed8
Andrés Elí Maciel Muñiz <tecno_eli@hotmail.com>
2021-01-03 00:05:24 -0600

    comando reloadSFW agregado

--- comandos/sfwreload.js
+++ comandos/sfwreload.js
@@ -7,6 +7,7 @@ module.exports = {
         try {
             if(![""].includes(message.author.id)) return;
             require('../sfw').loadsfw(["cry", "clap", "hug"], client);
+            message.channel.send("cargados");
         } catch (error) {
             client.channels.cache.get('795025963406458900').send(`Error en **"sfwreload"** <@&795025257157230643>\n${error.toString()}\nMensaje: ${message.content}\nCanal: <#${message.channel.id}>\nServidor ${message.guild.name} / ${message.guild.id}`);
             console.log(error);
