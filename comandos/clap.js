41c0887a2f5a07f09273efa8c421510943348965
lord-pixc <74274540+lord-pixc@users.noreply.github.com>
2021-01-02 20:06:05 -0600

    prueba de comando aplaude

---
+++ comandos/clap.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+const admin = require('firebase-admin');
+const {MessageEmbed} = require("discord.js");
+const admin = require('firebase-admin');
+const bd = admin.firestore();
+const package = require('../package.json')
+
+module.exports = {
+    nombre: "clap",
+    alias: ["aplaudir", "aplauso", "claps"],
+    run: async (client, message, args) => {
+        try {
+//obtenemos el array de cry
+            const gifs = client.sfw.get("claps");
+            //obtenemos un gif aleatorio
+            const gif = gifs[Math.floor(Math.random() * gifs.length)];
+            //crear el embed
+            const embed = new MessageEmbed()
+                .setColor(0x0041EE)//estalecemos el color
+                .setDescription(`${message.member} aplaude`)//estalecemos la descripción
+                .setImage(gif)//estalecemos el gif
+                .setAuthor(client.user.username, client.user.avatarURL())//estalecemos el autor
+                .setFooter(`TheLordBot ${package.version}`, client.user.avatarURL());//estalecemos el footer
+            message.channel.send(embed);//se envia el embed
+        } catch (error) {
+            client.channels.cache.get('795025963406458900').send(`Error en **"afiliados"** <@&795025257157230643>\n${error.toString()}\nMensaje: ${message.content}\nCanal: <#${message.channel.id}>\nServidor ${message.guild.name} / ${message.guild.id}`);
+            console.log(error);
+        }
+    }
+}
