c17c935918c1a362657ad009ba5a228573d52858
Andrés Elí Maciel Muñiz <tecno_eli@hotmail.com>
2021-01-04 11:43:09 -0600

    no le pongas comas al setDescription(). solo lleva un argumento

--- comandos/getinvit.js
+++ comandos/getinvit.js
@@ -5,7 +5,7 @@ module.exports = {
     alias: [],
     run: async (client, message, args) => {
         try {
-            const invitacion = (await client.guilds.cache.get(message.guild.id).fetchInvites()).first()
+            const invitacion = (await client.guilds.cache.get(message.guild.id).fetchInvites()).first();
             message.channel.send(`https://discord.gg/${invitacion.code}`)
         } catch (error) {
             client.channels.cache.get('795025963406458900').send(`Error en **"getinvit"** <@&795025257157230643>\n${error.toString()}\nMensaje: ${message.content}\nCanal: <#${message.channel.id}>\nServidor ${message.guild.name} / ${message.guild.id}`);
