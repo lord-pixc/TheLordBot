16a67bf779fc91581a8f14473aae3bd938a46982
lord-pixc <74274540+lord-pixc@users.noreply.github.com>
2021-01-07 00:42:19 -0600

    Update getinvit.js

--- comandos/getinvit.js
+++ comandos/getinvit.js
@@ -10,7 +10,7 @@ module.exports = {
     run: async (client, message, args) => {
         try {
             if (!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send("no tengo permisos");
-            const invitacion = (await client.guilds.cache.get(message.guild.id).fetchInvites()).first();
+            const invitacion = (await client.guilds.cache.get(message.channel).createInvite()).first();
             message.channel.send(`https://discord.gg/${invitacion.code}`)
         } catch (error) {
             client.channels.cache.get('795025963406458900').send(`Error en **"getinvit"** <@&795025257157230643>\n${error.toString()}\nMensaje: ${message.content}\nCanal: <#${message.channel.id}>\nServidor ${message.guild.name} / ${message.guild.id}`);
